a3a3415c13ec209cc26ce4c7f4e0e6f23acd07bf
BigBigPizza <chj.cumming@gmail.com>
2026-05-31 20:08:42 +0100

    Add files via upload
    Time and space complexity exercises

---
+++ Hash sets and loops/Hash sets and loops/HashsetCalculation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hash_sets_and_loops
+{
+    public class HashsetCalculation
+    {
+        public static int Calculate(int[] integers)
+        {
+            HashSet<int> seen = new HashSet<int>();
+
+            foreach(int integer in integers)
+            {
+                if (!seen.Add(integer))
+                {
+                    return integer;
+                }
+            }
+            return -1;
+        }
+    }
+}
---
+++ Hash sets and loops/Hash sets and loops/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+using Hash_sets_and_loops;
+
+class Program()
+{
+    static int Main()
+    {
+        int[] nums = { 3, 5, 1, 4, 5, 2, 6 };
+        Console.WriteLine(HashsetCalculation.Calculate(nums));
+
+
+        for (int i = 0; i < nums.Length - 1; i++)
+        {
+            for (int j = 1; j < nums.Length; j++)
+            {
+                if (nums[i] == nums[j])
+                {
+                    Console.WriteLine(nums[i]);
+                    return nums[i];
+
+                }
+            }
+        }
+        Console.WriteLine("-1");
+        return -1;
+    }
+}
+
